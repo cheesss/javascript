26c63e4742f43b02fdc8d1173b759ab43356ff85
cheesss <chohjender@gmail.com>
2022-10-29 11:00:12 +0900

    css연습

--- test2.js
+++ test2.js
@@ -1,1 +1,25 @@
-alert("hello")
+a = true
+if (a){
+    console.log(a)
+}
+
+const info = {
+    name: "조현준",
+    age: 21,
+    address: 'bangwha',
+    getPoint: function() {
+        return 100
+    }
+}
+const cho_info = info
+cho_info.name = '한채린'
+console.log(cho_info.name)
+
+function cat(){
+    this.name = 'kitty'
+    this.address = '오목교'
+}
+
+const lyn_cat = new cat()
+console.log(lyn_cat.name)
+
